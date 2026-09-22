631cffaee7d34656a2281e26c434f3d996f8ed74
Oshi2000 <oshadirangika2300@gmail.com>
2026-09-22 23:26:15 +0530

    Add checkout page object

---
+++ Pages/CheckoutPage.js
@@ -0,0 +1,33 @@
+class CheckoutPage {
+  constructor(page) {
+    this.page = page;
+    this.firstNameInput = '#first-name';
+    this.lastNameInput = '#last-name';
+    this.postalCodeInput = '#postal-code';
+    this.continueButton = '#continue';
+    this.finishButton = '#finish';
+    this.completeHeader = '.complete-header';
+    this.summaryItems = '.cart_item';
+  }
+
+  async fillInformation(firstName, lastName, postalCode) {
+    await this.page.fill(this.firstNameInput, firstName);
+    await this.page.fill(this.lastNameInput, lastName);
+    await this.page.fill(this.postalCodeInput, postalCode);
+    await this.page.click(this.continueButton);
+  }
+
+  async getSummaryItemsCount() {
+    return await this.page.locator(this.summaryItems).count();
+  }
+
+  async completeOrder() {
+    await this.page.click(this.finishButton);
+  }
+
+  async getConfirmationMessage() {
+    return await this.page.textContent(this.completeHeader);
+  }
+}
+
+module.exports = { CheckoutPage };
